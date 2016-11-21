14b8f442a3c60b363229bc34682c9fcc1765f1a3
Chris Tessum <ctessum@gmail.com>
2016-11-21 14:54:35 -0800

    Added time

---
+++ time.go
@@ -0,0 +1,48 @@
+package eaprm
+
+import "time"
+
+// Hour returns a two dimensional array of 1 or zero values, where
+// the first dimension of the array is of length 24 and corresponds
+// to the hours of the day, and the second dimension of the array is
+// the same length as t. Values in the returned array [i,j] will be
+// 1 if the hour of day in t[j] equals i, and zero otherwise.
+func Hour(t []time.Time) [][]float64 {
+	o := make([][]float64, 24)
+	for i := range o {
+		o[i] = make([]float64, len(t))
+	}
+	for j, tt := range t {
+		o[tt.Hour()][j] = 1
+	}
+	return o
+}
+
+// Month returns a two dimensional array of 1 or zero values, where
+// the first dimension of the array is of length 12 and corresponds
+// to the months of the year, and the second dimension of the array is
+// the same length as t. Values in the returned array [i,j] will be
+// 1 if the month in t[j] equals i, and zero otherwise. January is month 0.
+func Month(t []time.Time) [][]float64 {
+	o := make([][]float64, 24)
+	for i := range o {
+		o[i] = make([]float64, len(t))
+	}
+	for j, tt := range t {
+		o[int(tt.Month())-1][j] = 1
+	}
+	return o
+}
+
+// Weekend returns an array of 1 or zero values, with 1 values for
+// members of t that are on a weekend (i.e., Saturday or Sunday) and
+// zero otherwise.
+func Weekend(t []time.Time) []float64 {
+	o := make([]float64, len(t))
+	for j, tt := range t {
+		if w := tt.Weekday(); w == time.Saturday || w == time.Sunday {
+			o[j] = 1
+		}
+	}
+	return o
+}
---
+++ time_test.go
@@ -0,0 +1,74 @@
+package eaprm
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestHour(t *testing.T) {
+	times := []time.Time{
+		time.Date(1969, time.July, 20, 23, 0, 0, 0, time.UTC),
+		time.Date(1929, time.October, 24, 3, 0, 0, 0, time.UTC),
+		time.Date(1066, time.October, 14, 11, 0, 0, 0, time.UTC),
+	}
+	hours := Hour(times)
+	sum := 0.0
+	for _, h := range hours {
+		for _, hh := range h {
+			sum += hh
+		}
+	}
+	if sum != 3 {
+		t.Errorf("sum: have %g but want %d", sum, 3)
+	}
+	if hours[23][0] != 1 {
+		t.Error("times[23][0] should equal 1")
+	}
+	if hours[3][1] != 1 {
+		t.Error("times[3][1] should equal 1")
+	}
+	if hours[11][2] != 1 {
+		t.Error("times[11][2] should equal 1")
+	}
+}
+
+func TestMonth(t *testing.T) {
+	times := []time.Time{
+		time.Date(1969, time.July, 20, 23, 0, 0, 0, time.UTC),
+		time.Date(1929, time.October, 24, 3, 0, 0, 0, time.UTC),
+		time.Date(1066, time.October, 14, 11, 0, 0, 0, time.UTC),
+	}
+	months := Month(times)
+	sum := 0.0
+	for _, m := range months {
+		for _, mm := range m {
+			sum += mm
+		}
+	}
+	if sum != 3 {
+		t.Errorf("sum: have %g but want %d", sum, 3)
+	}
+	if months[6][0] != 1 {
+		t.Error("times[6][0] should equal 1")
+	}
+	if months[9][1] != 1 {
+		t.Error("times[9][1] should equal 1")
+	}
+	if months[9][2] != 1 {
+		t.Error("times[9][2] should equal 1")
+	}
+}
+
+func TestWeekend(t *testing.T) {
+	times := []time.Time{
+		time.Date(1969, time.July, 20, 23, 0, 0, 0, time.UTC),
+		time.Date(1929, time.October, 24, 3, 0, 0, 0, time.UTC),
+		time.Date(1066, time.October, 14, 11, 0, 0, 0, time.UTC),
+	}
+	weekend := Weekend(times)
+	want := []float64{1, 0, 1}
+	if !reflect.DeepEqual(weekend, want) {
+		t.Errorf("weekend: have %v, want %v", weekend, want)
+	}
+}
